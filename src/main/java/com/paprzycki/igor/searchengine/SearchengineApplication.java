2dbd8d42cb8b4bf562f3e8bbcc198a6b0ea0e4b2
Igor Paprzycki <igypap@gmail.com>
2018-02-20 12:07:45 +0100

    SearchEngine - initial commit

---
+++ src/main/java/com/paprzycki/igor/searchengine/SearchengineApplication.java
@@ -0,0 +1,12 @@
+package com.paprzycki.igor.searchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchengineApplication.class, args);
+	}
+}
